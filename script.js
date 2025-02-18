bcdb8ca25b6cae9fabca64dfaf70a1b24ff44fb6
Juan Zabala <juanpablozabla@gmail.com>
2025-02-18 09:54:07 -0500

    ejercicios 1 y 2

---
+++ script.js
@@ -0,0 +1,16 @@
+//ejercicio mental 1
+let nombre = "José Manuel"
+let edad = 23
+let ciudad = "Guagira"
+console.log ("mi nombre es " + nombre + ", tengo " + edad + " y vivo en " + ciudad + ".")
+
+/* Crea una variable llamada precio y asígnale un valor decimal.
+luego, crea otra variable llamada cantidad y asígnale un valor entero.
+muestra el tipo de dato de cada una variable en la consola*/
+
+let precio = 2.594
+let cantidad = 5
+console.log (typeof precio, precio) //imprime y muestra el numero
+console.log (typeof cantidad, cantidad) //imprime y muestra el numero
+document.write (typeof cantidad)
+alert (typeof cantidad)
